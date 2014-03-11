bd5e46441fc29d6459b44abab30f33931e9c0343
aert <dev.aert@gmail.com>
2014-03-11 20:37:02 +0100

    Home : draft.

--- bee3/app.py
+++ bee3/app.py
@@ -1,14 +1,25 @@
+# coding=utf-8
 from flask import Flask, render_template
-from flask.ext.babel import Babel
 
 
 app = Flask(__name__)
 app.config.from_object("bee3.config")
-babel = Babel(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("frontend/index.html")
+    articles = [
+        {"title": u"Porte caisse facom", "price": "30", "desc": u"Strasbourg / Bas-Rhin"},
+        {"title": u"Monnaies: Médailles attribuées", "price": "10", "desc": u"Lyon / Rhône"},
+        {"title": u"Golf 4 GTI TDI 110 cuir beige distri neuve carnet", "price": "4 000", "desc": u"Shiltigheim / Bas-Rhin"},
+        {"title": u"Canaper 2 place +fauteuil on peut ouvrir les bas", "price": "300", "desc": u"Strasbourg / Bas-Rhin"},
+        {"title": u"Demain est un autre jour LARRY COLLINS", "price": " 5", "desc": u"Strasbourg / Bas-Rhin"},
+        {"title": u"Xbox one + FIFA 14, garantie 3 ans + live 1 an", "price": "500", u"desc": u"Haguenau / Bas-Rhin"},
+        {"title": u"Verrines jetables forme tube \"FIFINE\"", "price": "1", "desc": u"Strasbourg / Bas-Rhin"},
+        {"title": u"Anciens livres brochés", "price": "", "desc": u"Strasbourg / Bas-Rhin"},
+        {"title": u"Moteur 350 rdlc 31k", "price": "800", "desc": u"Strasbourg / Bas-Rhin"},
+    ]
+    return render_template("frontend/index.html",
+                           articles=articles)
 
 
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
-
+#!/usr/bin/env python
 from bee3.app import app
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run('0.0.0.0', debug=True)
