1818200e63995fb4fb638444022ceeb15c57ee11
aert <dev.aert@gmail.com>
2014-03-11 20:54:08 +0100

    WSGI wrapper.

---
+++ bee3/wsgi.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from werkzeug.contrib.fixers import ProxyFix
+from .app import app
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
